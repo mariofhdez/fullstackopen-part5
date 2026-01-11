42291ce6644c6a27ad97c931f2e7d1c818866477
Mariofhdez <mario.florez249@gmail.com>
2026-01-11 13:19:00 -0500

    Part5: exercise 5.2

--- src/App.jsx
+++ src/App.jsx
@@ -14,41 +14,69 @@ function App() {
     blogService.getAll().then(res => {
       setBlogs(res)
     })
-  },[])
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
 
   const handleLogin = async (e) => {
     e.preventDefault()
-    const user = await loginService.login({ username, password })
-    console.log('Iniciando sesión', username);
-    setUser(user)
+    try {
+      const user = await loginService.login({ username, password })
+
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      console.error('Error', error.message);
+    }
+  }
+
+  const handleLogout = async (e) => {
+    e.preventDefault()
+    try {
+      window.localStorage.clear()
+  
+      setUser(null)
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      console.error('Error', error.message);
+    }
   }
 
   const loginForm = () => (
     <>
-    <h2>Login</h2>
-    <form onSubmit={handleLogin}>
-      <div style={{'display': 'flex', 'alignItems': 'center', 'gap':"15px"}}>
-        <p>Username</p> 
-        <input 
-          style={{'height':'20px'}}
-          type="text" 
-          value={username}
-          name='Username'
-          onChange={({target}) => setUsername(target.value)}
-        />
-      </div>
-      <div style={{'display': 'flex', 'alignItems': 'center', 'gap':"15px"}}>
-        <p>Password</p> 
-        <input 
-          style={{'height':'20px'}}
-          type="password" 
-          value={password}
-          name='Password'
-          onChange={({target}) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">Login</button>
-    </form>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div style={{ 'display': 'flex', 'alignItems': 'center', 'gap': "15px" }}>
+          <p>Username</p>
+          <input
+            style={{ 'height': '20px' }}
+            type="text"
+            value={username}
+            name='Username'
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div style={{ 'display': 'flex', 'alignItems': 'center', 'gap': "15px" }}>
+          <p>Password</p>
+          <input
+            style={{ 'height': '20px' }}
+            type="password"
+            value={password}
+            name='Password'
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </>
   )
 
@@ -58,8 +86,12 @@ function App() {
       {user === null ?
         loginForm() :
         <div>
+          <div>
+            <p>{user.name}</p>
+            <button onClick={handleLogout}>Log out</button>
+          </div>
           <h2>Blog list</h2>
-          {blogs.map(b =><Blog key={b.id} blog={b} />)}
+          {blogs.map(b => <Blog key={b.id} blog={b} />)}
         </div>
       }
     </>

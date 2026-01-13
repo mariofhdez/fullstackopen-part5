24a27e617c106f5b5704f9e80c2ec78c32203b0c
Mariofhdez <mario.florez249@gmail.com>
2026-01-13 00:01:10 -0500

    Part5: exercise 5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,32 @@
-const Blog = ( {blog} ) => {
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+    const [visible, setVisible] = useState(false)
+
+    const showWhenVisible = { display: visible ? '' : 'none' }
+
+    const toggleVisibility = () => {
+        setVisible(!visible)
+    }
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
     return (
-        <div>
-            <p><span>{blog.title}</span> - {blog.author}</p>
+        <div style={blogStyle}>
+            <div>
+                <p><span>{blog.title}</span> - {blog.author} <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button></p>
+            </div>
+            <div style={showWhenVisible}>
+                <p>{blog.url}</p>
+                <p>Likes: {blog.likes} <button>like</button></p>
+                <p>{blog.user ? blog.user.name : ''}</p>
+            </div>
         </div>
     )
 }

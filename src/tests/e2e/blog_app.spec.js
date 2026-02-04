aa2d27a0f7cbb581354f979a1b3713ce35bbc378
Mariofhdez <mario.florez249@gmail.com>
2026-02-03 23:04:25 -0500

    Part5: exercise 5.18

--- src/tests/e2e/blog_app.spec.js
+++ src/tests/e2e/blog_app.spec.js
@@ -1,8 +1,17 @@
 import { test, expect } from '@playwright/test'
+import { loginWith } from './helper'
 
 test.describe('Blog app', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173')
+  test.beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'John Doe',
+        username: 'jdoe',
+        password: 'password',
+      },
+    })
+    await page.goto('/')
   })
 
   test('login form is shown', async ({ page }) => {
@@ -10,5 +19,23 @@ test.describe('Blog app', () => {
     await expect(page.getByTestId('username')).toBeVisible()
     await expect(page.getByTestId('password')).toBeVisible()
     await expect(page.getByText('Log in')).toBeVisible()
+  })
+
+  test('user can log in', async ({ page }) => {
+    await loginWith(page, 'jdoe', 'password')
+
+    await expect(page.getByText('John Doe')).toBeVisible()
+  })
+
+  test('login fails with wrong password', async ({ page }) => {
+    await loginWith(page, 'jdoe', 'wrong')
+
+    const errorMessageDiv = page.locator('.error')
+
+    await expect(errorMessageDiv).toContainText('Wrong username or password')
+    await expect(errorMessageDiv).toHaveCSS('border-style', 'solid')
+    await expect(errorMessageDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+
+    await expect(page.getByText('John Doe')).not.toBeVisible()
   })
 })
